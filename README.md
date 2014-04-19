e4c560f290b3be7e0fe53172fbc68abe313cb755
Mauro Bolis <bolismauro@gmail.com>
2014-04-19 13:09:59 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Meethub website
+Thanks to Emanuele Brivio - [emanuele@brivio.me](mailto:emanuele@brivio.me)
---
+++ static/js/main.js
@@ -0,0 +1,18 @@
+/*jshint browser:true, indent:2, laxcomma:true, loopfunc: true */
+/*global NodeList, HTMLCollection */
+
+$(function () {
+  'use strict';
+
+  $(window).scroll(function() {
+    var s = $(window).scrollTop()
+      , height = $('.container').height() * 5
+      , opacityVal = Math.max(0, 1 - (s / height));
+
+    $('.blur').css('opacity', opacityVal);
+  });
+
+
+  $('.imagine').delay(500).fadeIn(800);
+
+});
